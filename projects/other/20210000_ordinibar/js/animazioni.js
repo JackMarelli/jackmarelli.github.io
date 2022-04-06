351643d1f929b989b764ce2d885bfc0810155735
JackMarelli <marelligiacomo@gmail.com>
2022-04-06 09:44:14 +0200

    sparito progetti inutili

--- projects/other/20210000_ordinibar/js/animazioni.js
+++
@@ -1,5 +0,0 @@
-function loginOverlayDisappear() {
-    let overlay = document.getElementById("login-overlay");
-    overlay.style.opacity = 0;
-    overlay.style.visibility = "hidden";
-}
--- projects/other/20220114_sunsetgradient/js/onload.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('load', (event) => {
-    setTimeout(() => {  document.getElementById("sky").setAttribute("class", "sunset-gradient"); }, 1000);
-    console.log("page loaded and 1 second waited");
-});
