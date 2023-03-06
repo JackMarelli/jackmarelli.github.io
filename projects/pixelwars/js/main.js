44b85bbab2761f441789448d0e9814198470282e
JackMarelli <marelligiacomo@gmail.com>
2023-03-06 19:55:52 +0100

    tanta roba

--- projects/pixelwars/js/main.js
+++ projects/pixelwars/js/main.js
@@ -1,22 +1,20 @@
+import Unit from "./classes.js"
+import "./utils.js"
+import { getRandomHexColor } from "./utils.js";
+
 const _app = {};
 
 //alphabetical order
 _app.canvasElement = document.querySelector("#canvasElement");
 _app.colors = ["#FF0000", "#00FF00", "#0000FF", "FFFF00"];
 _app.ctx = _app.canvasElement.getContext("2d");
-_app.divisions = 100;
+_app.divisions = 10;
 _app.matrix = [];
 _app.size = 500;
 _app.step = _app.size / _app.divisions;
 
-_app.getRandomInt = (max) => {
-    return Math.floor(Math.random() * max);
-  };
-  
-
 _app.setup = () => {
-  //get canvas and ctx
-
+  //create matrix
   for (var i = 0; i < _app.divisions; i++) {
     _app.matrix[i] = new Array(_app.divisions);
   }
@@ -26,7 +24,7 @@ _app.setup = () => {
   _app.stepY = 0;
   for (let i = 0; i < _app.divisions; i++) {
     for (let j = 0; j < _app.divisions; j++) {
-      _app.matrix[i][j] = [_app.stepX, _app.stepY];
+      _app.matrix[i][j] = new Unit(_app.stepX, _app.stepY, "#000000");
       _app.stepX += _app.step;
     }
     _app.stepX = 0;
@@ -34,20 +32,74 @@ _app.setup = () => {
   }
 };
 
+_app.drawUnit = (u) => {
+  _app.ctx.fillStyle = u.color;
+  _app.ctx.fillRect(
+    u.x,
+    u.y,
+    _app.step,
+    _app.step
+  );
+}
+
 _app.randomizeMatrix = () => {
+  console.log("randomizing matrix");
+  let stepX = 0, stepY = 0;
   for (let i = 0; i < _app.divisions; i++) {
     for (let j = 0; j < _app.divisions; j++) {
-      _app.ctx.fillStyle = _app.colors[_app.getRandomInt(_app.colors.length -1)];
-      _app.ctx.fillRect(
-        _app.matrix[i][j][0],
-        _app.matrix[i][j][1],
-        _app.step,
-        _app.step
-      );
+      _app.matrix[i][j] = new Unit(stepX, stepY, getRandomHexColor());
+      stepY += _app.step;
+      _app.drawUnit(_app.matrix[i][j]);
     }
+    stepY = 0;
+    stepX += _app.step;
   }
 };
 
+_app.initializeMatrix = () => {
+  _app.color1 = getRandomHexColor()
+  _app.color2 = getRandomHexColor()
+  let stepX = 0;
+  let stepY = 0;
+
+  //left side
+  for (let i = 0; i < _app.divisions / 2; i++) {
+    for (let j = 0; j < _app.divisions; j++) {
+      _app.matrix[i][j] = new Unit(stepX, stepY, _app.color1);
+      stepY += _app.step;
+      _app.drawUnit(_app.matrix[i][j]);
+    }
+    stepY = 0;
+    stepX += _app.step;
+  }
+
+  //right side
+  for (let i = 0; i < _app.divisions / 2; i++) {
+    for (let j = 0; j < _app.divisions; j++) {
+      _app.matrix[i][j] = new Unit(stepX, stepY, _app.color2);
+      stepY += _app.step;
+      _app.drawUnit(_app.matrix[i][j]);
+    }
+    stepY = 0;
+    stepX += _app.step;
+  }
+
+}
+
+_app.infect = () => {
+  for (let i = 0; i < _app.divisions; i++) {
+    for (let j = 0; j < _app.divisions; j++) {
+      if ((_app.matrix[i + 1][j] != undefined)) {
+        if (_app.matrix[i][j].color != _app.matrix[i + 1][j].color){
+          if (!(_app.matrix[i][j].fight(_app.matrix[i + 1][j]))) {
+            _app.drawUnit(_app.matrix[i][j]);
+          }
+        }
+      }
+    }
+  }
+}
+
 _app.setup();
-_app.randomizeMatrix();
-console.log(_app.matrix)
+_app.initializeMatrix();
+_app.infect();
