2cb19c9393ea0adb9c6b14c9e62d6dbfb1efaa07
JackMarelli <marelligiacomo@gmail.com>
2023-03-11 21:56:03 +0100

    portfolio v1.0 senza link senza about

--- projects/portfolio/js/main.js
+++ projects/portfolio/js/main.js
@@ -1,35 +1,30 @@
 const scroller = document.querySelector("#scroller");
 const cursorDiv = document.querySelector("#cursorDiv");
 const sampleScrollerElement = document.querySelector("#sampleScrollerElement");
+const scrollerElements = document.getElementsByClassName("img-container");
 let sx = 0;
 let dx = sx;
 
-
 window.addEventListener("wheel", (e) => {
-  sx += e.deltaY;
+  moveCursor(e);
   if (sx > scroller.scrollWidth) {
     sx = scroller.scrollWidth;
-  } else if (sx <= 0) {
+  } else if (sx < 0) {
     sx = 0;
+  } else {
+    sx += e.deltaY;
   }
 });
 
 window.requestAnimationFrame(render);
 function render() {
-  //We calculate our container position by linear interpolation method
-  dx = li(dx, sx, 0.05);
+  dx = li(dx, sx, 0.04);
   dx = Math.floor(dx * 100) / 100;
 
-  if (dx > scroller.scrollWidth - sampleScrollerElement.offsetWidth) {
-    dx = scroller.scrollWidth - sampleScrollerElement.offsetWidth;
-  } else if (dx <= 0) {
-    dx = 0;
-  }
+  //console.log(dx);
+  //onsole.log(scroller.scrollWidth);
 
-  console.log(dx)
   scroller.scrollLeft = dx;
-  console.log(scroller.scrollWidth);
-
   window.requestAnimationFrame(render);
 }
 
@@ -37,7 +32,29 @@ function li(a, b, n) {
   return (1 - n) * a + n * b;
 }
 
+// Cursor
+window.onload = () => {
+  cursorDiv.classList.add("cursorDefault");
+}
+
 window.addEventListener("mousemove", (e) => {
+moveCursor(e)});
+
+for (let i = 0; i < scrollerElements.length; i++) {
+  console.log("event listeners added")
+  scrollerElements[i].addEventListener("mouseenter", (e) => {
+    console.log("fired " + e.type);
+    cursorDiv.classList.add("cursorCta");
+    cursorDiv.classList.remove("cursorDefault");
+  });
+  scrollerElements[i].addEventListener("mouseleave", (e) => {
+    console.log("fired " + e.type);
+    cursorDiv.classList.add("cursorDefault");
+    cursorDiv.classList.remove("cursorCta");
+  });
+}
+
+function moveCursor(e) {
   cursorDiv.style.top = `${e.clientY - cursorDiv.offsetWidth / 2}`;
   cursorDiv.style.left = `${e.clientX - cursorDiv.offsetHeight / 2}`;
-});
+}
