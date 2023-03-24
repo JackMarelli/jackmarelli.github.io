84bcc19cd9628bd87bfd8f20e3c76c6e87206411
JackMarelli <marelligiacomo@gmail.com>
2023-03-24 17:24:13 +0100

    fixed horizontal overflow, fixed font sizes and layout

--- projects/csc/website/js/main.js
+++ projects/csc/website/js/main.js
@@ -1,21 +1,39 @@
 console.log("main.js loads");
+const _app = {}
 
-$(document).ready(function () {
+_app.owlCarouselSetup = () => {
+  $(document).ready(function () {
     $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 30,
-        nav: false,
-        stagePadding: 30,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 2
-            },
-            1100: {
-                items: 4
-            }
+      loop: true,
+      margin: 50,
+      nav: false,
+      stagePadding: 50,
+      responsive: {
+        0: {
+          items: 1,
+          margin: 30
+        },
+        600: {
+          items: 2
+        },
+        1100: {
+          items: 3
+        },
+        1400: {
+          items: 4
         }
+      }
     });
-});
+  });
+};
+
+_app.reviewsSetup = () => {
+  //todo: randomly picked reviews
+}
+
+_app.startUp = () => {
+  _app.owlCarouselSetup();
+  _app.reviewsSetup();
+}
+
+_app.startUp();
