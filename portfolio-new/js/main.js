ac99653caab9d2f98eb86fe22a16853144379618
JackMarelli <marelligiacomo@gmail.com>
2023-05-11 15:11:32 +0200

    aiuto

--- portfolio-new/js/main.js
+++ portfolio-new/js/main.js
@@ -2,12 +2,21 @@ const _app = {};
 
 _app.helloDiv = document.querySelector("#hello");
 _app.hellos = ["Hello", "Ciao", "Hola", "Alo", "Ave", "Salut", "Ahoj", "Privet", "Namaste", "Halo", "Shalom", "Nadzar", "Nin Hao"];
+_app.vHeigth = window.innerHeight;
+
+_app.setFoldHeight = () => {
+    console.log("setting fold height");
+    let decoHeight = document.querySelector("#fold_deco_img").outerHeight;
+    let navHeight = document.querySelector("#nav").outerHeight;
+    let customFoldHeight = _app.vHeigth - decoHeight / 2 - navHeight;
+    document.querySelector("#fold").style = height = `${customFoldHeight}px`;
+}
 
 _app.initHelloCarousel = () => {
-    let helloIndex = 0; 
+    let helloIndex = 0;
     setInterval(() => {
         _app.helloDiv.innerHTML = _app.hellos[helloIndex];
-        if (helloIndex < _app.hellos.length-1){
+        if (helloIndex < _app.hellos.length - 1) {
             helloIndex++
         } else {
             helloIndex = 0;
@@ -17,6 +26,12 @@ _app.initHelloCarousel = () => {
 
 _app.startUp = () => {
     _app.initHelloCarousel();
+    _app.setFoldHeight();
+}
+
+_app.handleResize = () => {
+    _app.setFoldHeight();
 }
 
 _app.startUp();
+window.onresize = () => _app.handleResize();
