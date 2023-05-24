d15da9305e548861c063d2397f6e378de47d8d94
JackMarelli <marelligiacomo@gmail.com>
2023-05-24 16:02:56 +0200

    dynamic navbar loading, image shake, about max width

--- js/main.js
+++ js/main.js
@@ -2,45 +2,41 @@ const _app = {};
 
 _app.helloDiv = document.querySelector("#hello");
 _app.hellos = ["Hello", "Ciao", "Hola", "Alo", "Ave", "Salut", "Ahoj", "привіт", "नमस्ते", "Halo", "שלום", "Nadzar", "你好" ,"здрасти", "안녕하세요", "سلام", "ਸਤ ਸ੍ਰੀ ਅਕਾਲ", "Здраво", "สวัสดี", "こんにちは", "გამარჯობა"];
-_app.vHeigth = window.innerHeight;
-
-_app.setFoldHeight = () => {
-    //console.log("setting fold height");
-    let decoHeight = document.querySelector("#fold_deco_img").outerHeight;
-    let navHeight = document.querySelector("#nav").outerHeight;
-    let customFoldHeight = _app.vHeigth - decoHeight / 2 - navHeight;
-    document.querySelector("#fold").style = height = `${customFoldHeight}px`;
-}
+_app.navTag = document.querySelector("nav");
+_app.pageName = document.querySelector("#pageName").innerHTML;
+_app.defaultMenuIconPath = "assets/icons/menu.svg";
 
 _app.initHelloCarousel = () => {
-    let helloIndex = Math.floor(Math.random() * _app.hellos.length);;
-    setInterval(() => {
-        _app.helloDiv.innerHTML = _app.hellos[helloIndex];
-        if (helloIndex < _app.hellos.length - 1) {
-            helloIndex++
-        } else {
-            helloIndex = 0;
-        }
-    }, 800);
+    if (_app.helloDiv) {
+        let helloIndex = Math.floor(Math.random() * _app.hellos.length);;
+        setInterval(() => {
+            _app.helloDiv.innerHTML = _app.hellos[helloIndex];
+            if (helloIndex < _app.hellos.length - 1) {
+                helloIndex++
+            } else {
+                helloIndex = 0;
+            }
+        }, 800);
+    }
+}
+
+_app.loadNav = (pageName, pathToMenuIcon) => {
+    if (_app.navTag) {
+        console.log('_app.pageName', _app.pageName);
+        console.log('_app.defaultMenuIconPath', _app.defaultMenuIconPath);
+        
+        _app.navTag.innerHTML  = `<div><a id="jm" class="link-dark" href="https://jackmarelli.com">JM</a><a id="jackmarelli" class="link-dark" href="https://jackmarelli.com">Jack Marelli</a><span class="nav-separator">-</span><span>${pageName}</span></div><div><img class="d-none" src="${pathToMenuIcon}" alt="Menu" /></div>`;
+    }
 }
 
 _app.startUp = () => {
     _app.initHelloCarousel();
-    //_app.setFoldHeight();
-}
 
-_app.handleResize = () => {
-    //_app.setFoldHeight();
+    if( _app.pageName != "Home") {
+        _app.defaultMenuIconPath = "../" + _app.defaultMenuIconPath;
+    }
+
+    _app.loadNav(_app.pageName, _app.defaultMenuIconPath);
 }
 
 _app.startUp();
-window.onresize = () => _app.handleResize();
-
-/*
-
- TODO:
- - immagine semi seria
- - aggiungere lavori
- - link social
-
-*/
