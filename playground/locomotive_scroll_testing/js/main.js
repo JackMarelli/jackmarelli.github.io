10f361cd101bacc1ec912c4c9b337824f387a61a
JackMarelli <marelligiacomo@gmail.com>
2023-06-07 16:53:29 +0200

    non va su mobile dp

--- playground/locomotive_scroll_testing/js/main.js
+++ playground/locomotive_scroll_testing/js/main.js
@@ -1,4 +1,18 @@
 const scroller = new LocomotiveScroll({
     el: document.querySelector("[data-scroll-container]"),
-    smooth: true
+    inertia: 0.8,
+    smooth: true,
+    getDirection: true,
+    mobile: {
+        inertia: 0.8,
+        breakpoint: 0,
+        smooth: false,
+        getDirection: true,
+    },
+    tablet: {
+        inertia: 0.8,
+        breakpoint: 0,
+        smooth: false,
+        getDirection: true,
+    },
 });
